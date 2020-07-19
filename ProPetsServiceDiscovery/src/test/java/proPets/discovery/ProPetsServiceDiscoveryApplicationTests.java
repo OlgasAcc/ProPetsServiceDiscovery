6da9942df2ff0f6670ac2d6c2cca701288d8fea9
olgak_tdvqm06 <olgak_tdvqm06@10.100.102.9>
2020-07-19 20:02:28 +0300

    First commit

---
+++ ProPetsServiceDiscovery/src/test/java/proPets/discovery/ProPetsServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package proPets.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPetsServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
